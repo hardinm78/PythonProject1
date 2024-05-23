11aa76db3a9c0e608cb24b3047360492c80b6c0c
Michael Hardin <hardinm78@gmail.com>
2024-05-23 10:33:20 -0500

    gui edit

--- gui.py
+++ gui.py
@@ -4,21 +4,34 @@ import FreeSimpleGUI as fsg
 label = fsg.Text("Type in a To-Do")
 input_box = fsg.InputText(tooltip="Enter To-Do", key="todo")
 add_button = fsg.Button("Add")
+edit_button = fsg.Button("Edit")
+list_box = fsg.Listbox(values=functions.read_todos_from_file(), key='todos', enable_events=True, size=(45, 10))
 
 window = fsg.Window("To-Do List",
-                    [[label, input_box, add_button]],
+                    [[label, input_box, add_button], [list_box, edit_button]],
                     font=('Helvetica', 14))
 
 while True:
     event, values = window.read()
     print(event)
-    print(values['todo'])
+    print(values)
 
     match event:
         case 'Add':
             todos = functions.read_todos_from_file()
-            todos.append(values['todo'] + '\n')
+            todos.append(str(values['todo'] + '\n').title())
+            list_box.update(todos)
             functions.save_todos_to_file(todos)
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = str(values['todo']+"\n").title()
+            todos = functions.read_todos_from_file()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            list_box.update(todos)
+            functions.save_todos_to_file(todos)
+        case 'todos':
+            input_box.update(value=values['todos'][0])
         case fsg.WINDOW_CLOSED:
             break
 
